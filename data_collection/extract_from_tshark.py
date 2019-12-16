7aa815e43d36e44c3d5925287bed1ee1071df285
Anastasia Shuba <ashuba@uci.edu>
2019-12-15 17:46:03 -0800

    update license

--- data_collection/extract_from_tshark.py
+++ data_collection/extract_from_tshark.py
@@ -5,6 +5,22 @@ Script used to extract only the needed information from JSON packet traces gener
 tshark from PCAPNG format
 """
 
+#  This file is part of NoMoATS <http://athinagroup.eng.uci.edu/projects/nomoads/>.
+#  Copyright (C) 2019 Anastasia Shuba.
+#
+#  NoMoATS is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  NoMoATS is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with NoMoATS.  If not, see <http://www.gnu.org/licenses/>.
+
 import os, sys
 import json, csv
 import uuid
--- nomoats/frida_net_layers.py
+++ nomoats/frida_net_layers.py
@@ -1,17 +1,18 @@
-# This file is part of AutoLabel <http://athinagroup.eng.uci.edu/projects/autolabel/>.
-# Copyright (C) 2019 Anastasia Shuba, University of California, Irvine.
+#  This file is part of NoMoATS <http://athinagroup.eng.uci.edu/projects/nomoads/>.
+#  Copyright (C) 2019 Anastasia Shuba.
 #
-# AutoLabel is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation version 2 of the License only.
+#  NoMoATS is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-# AutoLabel is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#  NoMoATS is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with AutoLabel.  If not, see <http://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU General Public License
+#  along with NoMoATS.  If not, see <http://www.gnu.org/licenses/>.
 
 IP_HEADER_DEFAULT_LENGTH = 20
 TCP_HEADER_DEFAULT_LENGTH = 20
